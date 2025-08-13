b90aeaed2f40e8112926c3715db632e7cd029878
laingyy <laing1324@gmail.com>
2025-08-14 00:11:54 +0100

    Add SignalR, EF Core, and initial migration
    Integrate SignalR for real-time communication, configure Entity Framework Core with PostgreSQL, add database context and initial migration. Update project files accordingly.

---
+++ Server/Database/ServerDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.Database
+{
+    public sealed class ServerDb : DbContext
+    {
+        public ServerDb(DbContextOptions<ServerDb> options) : base(options)
+        {
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Server.Database;
+
 namespace Server
 {
     public class Program
@@ -5,9 +10,34 @@ namespace Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.AddUserSecrets<Program>();
+
+            // Add services to the container.
+            builder.Services.AddCors(o => 
+            o.AddDefaultPolicy(p => 
+                p.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .SetIsOriginAllowed(_ => true)));
+
+            builder.Services.AddSignalR();
+
+            builder.Services.AddDbContext<ServerDb>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionStrings:DatabaseConnection")));
+
+            builder.Services.AddAuthentication().AddJwtBearer();
+
+            // builds services
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // Configure Pipeline
+            app.UseCors();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapGet("/health", () => Results.Ok( new { Ok = true }));
+
+            app.MapHub<ChatHub>("/Hubs/chat");
 
             app.Run();
         }
